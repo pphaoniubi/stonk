9ae2d2626acd60ad55725d645ae6fc2959168e89
Pai Peng <pphaoniubi@gmail.com>
2024-11-22 22:51:56 +0800

    no message

--- stonk_frontend/src/App.js
+++ stonk_frontend/src/App.js
@@ -1,17 +1,17 @@
-import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import AnnualReturns from './AnnualReturns';
 import StockMainPage from './StockMainPage';
+import "./App.css"
 
 function App() {
 
   return (
     <Router>
       <div>
-        <nav>
-          <Link to="/">Home</Link>
-          <Link to="/annual-returns">Annual Returns</Link>
-        </nav>
+      <nav className="navbar">
+        <Link to="/" className="nav-link">Home</Link>
+        <Link to="/annual-returns" className="nav-link">Annual Returns</Link>
+      </nav>
         
         <Routes>
           <Route path="/" element={<StockMainPage />} />
